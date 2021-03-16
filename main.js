3993c08ae9eb21fd3d8dcb139fe38b228edd7f16
Alamin-hossen <hossena863@gmail.com>
2021-03-16 19:47:41 +0600

    added bank deposit and balance

---
+++ main.js
@@ -0,0 +1,39 @@
+// login btn event handler.
+const enterBtn = document.getElementById('enter-btn');
+enterBtn.addEventListener('click',function(){
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display = 'none'
+    const trasactionArea = document.getElementById('transaction-area');
+    trasactionArea.style.display = 'block';
+})
+// deposit btn handler.
+const depositBtn = document.getElementById('deposit-btn');
+depositBtn.addEventListener('click',function(){
+    const depositNumber = getInputNumber ('deposit-amount');
+    updateInnerText('current-deposit', depositNumber);
+    updateInnerText("current-balance", depositNumber);
+    
+
+})
+// withdraw btn handler.
+const withdrawBtn = document.getElementById('add-withdraw');
+withdrawBtn.addEventListener('click', function (){
+    const withdrawNumber = getInputNumber('withdraw-amount');
+    console.log(withdrawNumber);
+})
+function getInputNumber(id) {
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
+}
+
+
+
+function updateInnerText(id, depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
+}
+
+  
